cb1994af364c7128d6fb281f037abb95f577d17f
robo <robolab1902@naver.com>
2022-06-15 19:32:13 +0900

    메모리 배리어 예제

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -6,37 +6,54 @@ namespace ServerCore
 {
     class Program
     {
+        static int x = 0;
+        static int y = 0;
+        static int r1 = 0;
+        static int r2 = 0;
+
+        // 1) Full Memory Barrier (ASM MFENCE, C# THread Memory Barrier) : Store, Load 둘다 막는다.
+        // 2) Store Memory Barrier (ASM SFENCE) : Store만 막는다.
+        // 3) Load Memory Barrier (ASM_LFENCE) : Load만 막는다.
+
+        static void Thread_1()
+        {
+            y = 1;  // Store y
+
+            Thread.MemoryBarrier();
+
+            r1 = x; // Load x
+        }
+
+        static void Thread_2()
+        {
+            x = 1;  // Store x
+
+            Thread.MemoryBarrier();
+
+            r2 = y; // Load y
+        }
+
         static void Main(string[] args)
         {
-            // 3 x 3 배열
-            // [][][] [][][] [][][]
-            int[,] arr = new int[10000, 10000];
-
+            int count = 0;
+            while (true)
             {
-                long now = DateTime.Now.Ticks;
-                for(int y=0; y<10000; y++)
+                x = y = r1 = r2 = 0;
+
+                Task t1 = new Task(Thread_1);
+                Task t2 = new Task(Thread_2);
+                t1.Start();
+                t2.Start();
+
+                Task.WaitAll(t1, t2);
+
+                if (r1 == 0 && r2 == 0)
                 {
-                    for(int x=0; x<10000; x++)
-                    {
-                        arr[y, x] = 1;
-                    }
+                    break;
                 }
-                long end = DateTime.Now.Ticks;
-                Console.WriteLine($"(y,x) 순서 걸린 시간 {end - now}");
             }
 
-            {
-                long now = DateTime.Now.Ticks;
-                for (int y = 0; y < 10000; y++)
-                {
-                    for (int x = 0; x < 10000; x++)
-                    {
-                        arr[x, y] = 1;
-                    }
-                }
-                long end = DateTime.Now.Ticks;
-                Console.WriteLine($"(x,y) 순서 걸린 시간 {end - now}");
-            }
+            Console.WriteLine($"{count}번만에 빠져나옴!");
         }
     }
 }
