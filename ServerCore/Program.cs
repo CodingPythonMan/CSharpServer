d50b406b9ef6123174b60677b0974eecc925cf9b
robo <robolab1902@naver.com>
2022-06-16 06:19:25 +0900

    ReaderWriterLock 구현, 재귀적 락 허용 X

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -6,54 +6,9 @@ namespace ServerCore
 { 
     class Program
     {
-        static object _lock = new object();
-        static SpinLock _lock2 = new SpinLock();
-
-        class Reward
-        {
-
-        }
-
-        // RWLock ReaderWriteLock
-        static ReaderWriterLockSlim _lock3 = new ReaderWriterLockSlim();
-
-        static Reward GetRewardById(int id)
-        {
-            _lock3.EnterReadLock();
-
-            _lock3.ExitReadLock();
-            return null;
-        }
-
-        static void AddReward(Reward reward)
-        {
-            _lock3.EnterReadLock();
-
-            _lock3.ExitReadLock();
-
-            lock (_lock)
-            {
-
-            }
-        }
-
         static void Main(string[] args)
         {
-            lock (_lock)
-            {
-
-            }
-
-            bool lockTaken = false;
-            try
-            {
-                _lock2.Enter(ref lockTaken);
-            }
-            finally
-            {
-                if (lockTaken)
-                    _lock2.Exit();
-            }
+            
         }
     }
 }
