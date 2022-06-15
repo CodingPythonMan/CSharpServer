7d44391c864f1ba1e8fda8db85466a3c8b9e4f97
robo <robolab1902@naver.com>
2022-06-16 07:09:54 +0900

    멀티쓰레드 기본기 완료, Thread Local Storage 예제 활용

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,22 +4,32 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 { 
+    // [ JobQueue ]
+
     class Program
     {
-        static ThreadLocal<string> ThreadName = new ThreadLocal<string>();
-        
+        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(() => { return $"My Name is {Thread.CurrentThread.ManagedThreadId}"; });
+
         static void WhoAmI()
         {
-            ThreadName.Value = $"My Name is {Thread.CurrentThread.ManagedThreadId}";
-
-            Thread.Sleep(1000);
-
-            Console.WriteLine(ThreadName.Value);
+            bool repeat = ThreadName.IsValueCreated;
+            if (repeat)
+            {
+                Console.WriteLine(ThreadName.Value + " {repeat}");
+            }
+            else
+            {
+                Console.WriteLine(ThreadName.Value);
+            }
         }
 
         static void Main(string[] args)
         {
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(3, 3);
             Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
+
+            ThreadName.Dispose();
         }
     }
 }
