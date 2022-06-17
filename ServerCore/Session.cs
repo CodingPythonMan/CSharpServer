2c7fde9a1dfcfb3b1c6c4531ddfd9ac41277bcd7
robo <robolab1902@naver.com>
2022-06-17 23:15:32 +0900

    RegisterRecv 구현 완료, 비동기 방식

---
+++ ServerCore/Session.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ServerCore
+{
+    class Session
+    {
+        Socket _socket;
+        int _disconnected = 0;
+
+        public void Start(Socket socket)
+        {
+            _socket = socket;
+            SocketAsyncEventArgs recvArgs = new SocketAsyncEventArgs();
+            recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
+            recvArgs.SetBuffer(new byte[1024], 0, 1024);
+
+            RegisterRecv(recvArgs);
+        }
+
+        public void Send(byte[] sendBuff)
+        {
+            SocketAsyncEventArgs sendArgs = new SocketAsyncEventArgs();
+            sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
+            sendArgs.SetBuffer(sendBuff, 0, sendBuff.Length);
+
+            RegisterSend(sendArgs);
+        }
+
+        public void Disconnect()
+        {
+            if(Interlocked.Exchange(ref _disconnected, 1) == 1)
+            {
+                return;
+            }
+
+            _socket.Shutdown(SocketShutdown.Both);
+            _socket.Close();
+        }
+
+        #region 네트워크 통신
+
+        void RegisterSend(SocketAsyncEventArgs args)
+        {
+            bool pending = _socket.SendAsync(args);
+            if(pending == false)
+            {
+                OnSendCompleted(null, args);
+            }
+        }
+
+        void OnSendCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            if(args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+            {
+                try
+                {
+
+                }catch(Exception e)
+                {
+                    Console.WriteLine($"OnSendCompleted Failed {e}");
+                }
+            }
+            else
+            {
+                Disconnect();
+            }
+        }
+
+        void RegisterRecv(SocketAsyncEventArgs args)
+        {
+            bool pending = _socket.ReceiveAsync(args);
+            if(pending == false)
+            {
+                OnRecvCompleted(null, args);
+            }
+        }
+
+        void OnRecvCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            try
+            {
+                if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+                {
+                    string recvData = Encoding.UTF8.GetString(args.Buffer, args.Offset, args.BytesTransferred);
+                    Console.WriteLine($"[From Client] {recvData}");
+                    RegisterRecv(args);
+                }
+                else
+                {
+                    Disconnect();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"OnRecvCompleted Failed {e}");
+            }
+        }
+        #endregion
+    }
+}
